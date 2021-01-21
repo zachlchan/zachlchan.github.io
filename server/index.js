65477af56638b610ca1edb4ec6da42c6e60f1732
Zach Chan <zacharyleechan@gmail.com>
2021-01-21 12:01:41 -0800

    moved bundle and index.html to root directory

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../')));
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
 });
