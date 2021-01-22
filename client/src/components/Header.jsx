e5231419941044d79c8ccf258a49c22ed060ccc6
Zach Chan <zacharyleechan@gmail.com>
2021-01-22 11:38:20 -0800

    removed background color and changed font color to dark olive green

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// tan backbround, grey text:
+// background-color: #dfd4bf;
+// color: #312f2b;
+
 const StyledHeader = styled.div`
   font-family: 'Work Sans', sans-serif;
   font-size: 3rem;
   font-weight: 500;
   letter-spacing: 0.5rem;
   text-align: center;
-  color: #312f2b;
-  background-color: #dfd4bf;
+  color: #556b2f;
   padding-top: 1.25rem;
   padding-bottom: 1.25rem;
 `;
