5b3036716903285ea737b1413536fa50c6d022e5
Zach Chan <zacharyleechan@gmail.com>
2021-01-21 17:57:10 -0800

    updated font-weight, size, and letter-spacing

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 
 const StyledHeader = styled.div`
   font-family: 'Work Sans', sans-serif;
-  font-size: 2rem;
+  font-size: 3rem;
+  font-weight: 500;
+  letter-spacing: 0.5rem;
   text-align: center;
   color: #312f2b;
   background-color: #dfd4bf;
