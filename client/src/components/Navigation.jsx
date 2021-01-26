e80973ee6bcea7cff4aac97638f99f65a286816f
Zach Chan <zacharyleechan@gmail.com>
2021-01-26 14:18:16 -0800

    refactored to used styled wrapper

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,56 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
 
-let displayValue;
-
 const Navigation = () => {
-
   return (
-    <StyledNav>
-      <StyledUl>
-        <StyledLi><StyledA href="#about">About</StyledA></StyledLi>
-        <StyledLi><StyledA href="#projects">Projects</StyledA></StyledLi>
-        <StyledLi><StyledA href="#skills">Skills</StyledA></StyledLi>
-        <StyledLi><StyledA href="#workexp">Work Experience</StyledA></StyledLi>
-        <StyledLi><StyledA href="#education">Education</StyledA></StyledLi>
-        <StyledLi><StyledA href="#contact">Contact</StyledA></StyledLi>
-      </StyledUl>
-    </StyledNav>
+    <StyledWrap>
+      <nav className="horizontal-nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a className="nav-link" href="#about">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#projects">Projects</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#skills">Skills</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#workexp">Work Experience</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#education">Education</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </StyledWrap>
   )
 }
 
-const StyledNav = styled.nav`
-  font-family: 'Work Sans', sans-serif;
-  text-transform: uppercase;
-  letter-spacing: .085rem;
-  //display: ${() => displayValue};
-  display: table;
-  @media (max-width: 768px) {
-    display: none;
+const StyledWrap = styled.div`
+  & .horizontal-nav {
+    font-family: 'Work Sans', sans-serif;
+    text-transform: uppercase;
+    letter-spacing: .085rem;
+    display: table;
+    margin: 0 auto;
+    padding-top: 0.625rem;
   }
-  margin: 0 auto;
-  padding-top: 0.625rem;
-`;
-const StyledUl = styled.ul`
-  list-style-type: none;
-  margin: 0;
-  padding 0;
-`;
-const StyledLi = styled.li`
-  display: inline;
-  list-style-type: none;
-  padding-left: .875rem;
-  padding-right: .875rem;
-`;
-const StyledA = styled.a`
-  position: relative;
-  text-decoration: none;
-  color: #312f2b;
-  &:hover {
+  & .nav-list {
+    list-style-type: none;
+    margin: 0;
+    padding 0;
+  }
+  & .nav-item {
+    display: inline-block;
+    list-style-type: none;
+  }
+  & .nav-link {
+    position: relative;
+    text-decoration: none;
+    color: #312f2b;
+  }
+  & .nav-link:hover {
     color: #312f2b;
     text-decoration: none;
   }
-  &:after {
+  & .nav-link:after {
     content: "";
     position: absolute;
     width: 100%;
@@ -62,9 +69,14 @@ const StyledA = styled.a`
     transition: transform 250ms ease-in-out;
     transform-origin: 50% 0%;
   }
-  &:hover:after {
+  & .nav-link:hover:after {
     transform: scaleX(1);
     transform-origin: 50% 0%;
+  }
+  @media (max-width: 768px) {
+    & .horizontal-nav {
+      display: none;
+    }
   }
 `;
 
