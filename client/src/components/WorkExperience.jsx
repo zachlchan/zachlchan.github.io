c4c5f70808e4c01c536845f849438e743bc04657
Zach Chan <zacharyleechan@gmail.com>
2021-01-25 18:55:45 -0800

    added download icon for resume

--- client/src/components/WorkExperience.jsx
+++ client/src/components/WorkExperience.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import SectionHeader from './SectionHeader.jsx';
 
 let ulFontSize;
@@ -21,19 +23,27 @@ const WorkExperience = ({ width }) => {
           <Date>2013-2016</Date>
           <Description>Ford Motor Company - KCAP Transit Body</Description>
         </StyledUl>
+        <StyledDiv>
+          <DownloadDiv>
+            <StyledA href="https://zc-portfolio.s3-us-west-1.amazonaws.com/Zach-Chan_Resume.pdf" download="zach-chan_resume">
+              <StyledIcon icon={faDownload} />
+              download resume
+            </StyledA>
+          </DownloadDiv>
+        </StyledDiv>
       </StyledSection>
     </div>
   )
 }
 
 const StyledSection = styled.section`
-  // border-top: 2px solid #dfd4bf;
   border-bottom: 2px solid #dfd4bf;
 `;
 const StyledUl = styled.ul`
   font-family: 'Work Sans', sans-serif;
   font-size: ${() => ulFontSize};
   font-weight: 500;
+  // display: inline-block;
   line-height: 1.5rem;
   color: #312f2b;
   margin-bottom: 2rem;
@@ -48,6 +58,31 @@ const Description = styled.li`
   font-family: 'Montserrat', sans-serif;
   font-size: ${() => liFontSize};
   list-style-type: none;
+`;
+const StyledDiv = styled.div`
+  text-align: center;
+`;
+const DownloadDiv = styled.div`
+  font-family: 'Work Sans', sans-serif;
+  display: inline-block;
+  background-color: #cf8a0c;
+  border-radius: 3px;
+  padding: 0.5rem;
+  margin: 1rem;
+  transition: transform 0.2s;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+const StyledA = styled.a`
+  text-decoration: none;
+  text-transform: uppercase;
+  color: #fff;
+`;
+const StyledIcon = styled(FontAwesomeIcon)`
+  color: #fff;
+  font-size: 1rem;
+  margin-right: 0.5rem;
 `;
 
 export default WorkExperience;
