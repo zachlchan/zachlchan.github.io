2075020092d43627fa022f667e95f8aed7884c0b
Zach Chan <zacharyleechan@gmail.com>
2021-01-26 23:08:45 -0800

    added className prop

--- client/src/components/SectionHeader.jsx
+++ client/src/components/SectionHeader.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SectionHeader = ({ text }) => {
+const SectionHeader = ({ className, text }) => {
   return (
-    <StyledHeader>
+    <StyledHeader className={className}>
       {text}
     </StyledHeader>
   )
