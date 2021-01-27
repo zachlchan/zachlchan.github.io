5bf90c9a0acf3d0a3e117de95667af4c19b44ead
Zach Chan <zacharyleechan@gmail.com>
2021-01-26 22:50:51 -0800

    refactored to styled wrapper and centered content

--- client/src/components/Education.jsx
+++ client/src/components/Education.jsx
@@ -4,56 +4,74 @@ import SectionHeader from './SectionHeader.jsx';
 
 const Education = () => {
   return (
-    <div id="education">
+    <StyledWrap id="education">
       <SectionHeader text="education" />
-      <StyledSection>
-        <StyledUl>Hack Reactor, San Francisco, California
-          <Date>2021</Date>
-          <Description>Advanced Software Engineering Immersive</Description>
-        </StyledUl>
-        <StyledUl>University of California, Los Angeles
-          <Date>2016</Date>
-          <Description>Master of Science, Mechanical Engineering</Description>
-        </StyledUl>
-        <StyledUl>The University of Texas at Austin
-          <Date>2012</Date>
-          <Description>Bachelor of Science, Mechanical Engineering</Description>
-        </StyledUl>
-      </StyledSection>
-    </div>
+      <div className="schools-container">
+        <div>
+          <ul className="school">
+            Hack Reactor, San Francisco, California
+            <li className="degree">
+              Advanced Software Engineering Immersive
+            </li>
+            <li className="date">2021</li>
+          </ul>
+          <ul className="school">
+            University of California, Los Angeles
+            <li className="degree">
+              Master of Science, Mechanical Engineering
+            </li>
+            <li className="date">2016</li>
+          </ul>
+          <ul className="school">
+            The University of Texas at Austin
+            <li className="degree">
+              Bachelor of Science, Mechanical Engineering
+            </li>
+            <li className="date">2012</li>
+          </ul>
+        </div>
+      </div>
+    </StyledWrap>
   )
 }
 
-const StyledSection = styled.section`
-  // border-top: 2px solid #dfd4bf;
+const StyledWrap = styled.div`
   border-bottom: 2px solid #dfd4bf;
-`;
-const StyledUl = styled.ul`
-  font-family: 'Work Sans', sans-serif;
-  font-size: 1.25rem;
-  font-weight: 500;
-  line-height: 1.5rem;
-  color: #312f2b;
-  margin-bottom: 2rem;
-  @media (max-width: 425px) {
+  & ul {
+    padding-inline-start: 0;
+  }
+  & .schools-container {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+  }
+  & .school {
+    font-family: 'Work Sans', sans-serif;
+    font-size: 1.25rem;
+    font-weight: 500;
+    line-height: 1.5rem;
+    color: #312f2b;
+    margin-bottom: 2rem;
+    @media (max-width: 425px) {
+      font-size: 1rem;
+    }
+  }
+  & .date {
+    font-family: 'Montserrat', sans-serif;
     font-size: 1rem;
+    color: #cf8a0c;
+    list-style-type: none;
+    @media (max-width: 425px) {
+      font-size: 0.875rem;
+    }
   }
-`;
-const Date = styled.li`
-  font-family: 'Montserrat', sans-serif;
-  font-size: 1rem;
-  color: #cf8a0c;
-  list-style-type: none;
-  @media (max-width: 425px) {
-    font-size: 0.875rem;
-  }
-`;
-const Description = styled.li`
-  font-family: 'Montserrat', sans-serif;
-  font-size: 1rem;
-  list-style-type: none;
-  @media (max-width: 425px) {
-    font-size: 0.875rem;
+  & .degree {
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1rem;
+    list-style-type: none;
+    @media (max-width: 425px) {
+      font-size: 0.875rem;
+    }
   }
 `;
 
