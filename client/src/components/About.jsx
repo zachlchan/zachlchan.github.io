8ec5b50bdb0ff5bc170b9c04cb5e68d25bde8b74
Zach Chan <zacharyleechan@gmail.com>
2021-01-26 19:46:36 -0800

    refactored to use styled wrapper

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -6,33 +6,40 @@ import SectionParagraph from './SectionParagraph.jsx';
 
 const About = () => {
   return (
-    <div>
-      <a id="about" style={{position: 'relative', top: '-192px', visibility: 'hidden'}} />
-      <StyledImg src="https://zc-portfolio.s3-us-west-1.amazonaws.com/profile-photo.jpeg" alt="profile photo" />
+    <StyledWrap>
+      <a id="about" />
+      <img className="profile-photo" src="https://zc-portfolio.s3-us-west-1.amazonaws.com/profile-photo.jpeg" alt="profile photo" />
       <SectionHeader text="about" />
-      <StyledSection>
+      <section className="description">
         <SectionParagraph text={aboutText} />
-      </StyledSection>
-    </div>
+      </section>
+    </StyledWrap>
   )
 }
 
 const aboutText = 'I am a full stack software engineer with a passion for hands-on problem solving and great design. With my background in mechanical engineering I excel at navigating complex systems and thrive in fast-paced, demanding environments.';
 
-const StyledSection = styled.section`
-  background-color: #dfd4bf;
-  color: #312f2b;
-  padding: 2rem;
-`;
-const StyledImg = styled.img`
-  display: block;
-  width: 75%;
-  margin: 0 auto 50px auto;
-  @media (max-width: 768px) {
-    margin: 10px auto 10px auto;
+const StyledWrap = styled.div`
+  & #about {
+    position: relative;
+    top: -192px;
+    visibility: hidden;
   }
-  @media (max-width: 425px) {
-    width: 100%
+  & .profile-photo {
+    display: block;
+    width: 75%;
+    margin: 0 auto 50px auto;
+    @media (max-width: 768px) {
+      margin: 10px auto 10px auto;
+    }
+    @media (max-width: 425px) {
+      width: 100%
+    }
+  }
+  & .description {
+    background-color: #dfd4bf;
+    color: #312f2b;
+    padding: 2rem;
   }
 `;
 
