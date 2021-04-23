2e7d0252752b50d5443e6f05841ab1861206b06c
Zach Chan <zacharyleechan@gmail.com>
2021-04-22 18:00:16 -0700

    updated porfolio project to not use NextGenIMG component in order to implement responsive sizes

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -96,15 +96,17 @@ const Projects = () => {
         <section className="flex-grid portfolio">
           <div className="single-img-container">
             <div className="img-container">
-              <NextGenIMG
-                className="portfolio-img"
-                srcWebp="https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail_mobile.webp 800w, https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail.webp 1024w"
-                sizes="(max-width:768px) 800px, 1024px"
-                srcJxr="https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail.jxr"
-                srcJp2="https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail.jp2"
-                fallback="https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail.jpeg"
-                alt="portfolio thumbnail"
-              />
+              <picture>
+                <source srcSet="https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail_mobile.webp 800w,
+                                https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail.webp 1024w"
+                        sizes="(max-width:768px) 800px,
+                              1024px"
+                        type="image/webp" />
+                <source srcSet="https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail.jxr" type="image/jxr" />
+                <source srcSet="https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail.jp2" type="image/jp2" />
+                <source srcSet="https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail.jpeg" type="image/jpeg" />
+                <img className="portfolio-img" src="https://zc-portfolio.s3-us-west-1.amazonaws.com/portfolio_thumbnail.jpeg" alt="portfolio thumbnail" />
+              </picture>
             </div>
           </div>
           <div className="project-container">
